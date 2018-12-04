e9950e86e19e529520e48c3ee9953bfcc1cbc106
Christian Stein <sormuras@gmail.com>
2018-12-04 11:28:10 +0100

    Add and implement Overlay.Log interface

--- junit-platform-manager-base-8/src/main/java/de/sormuras/junit/platform/manager/Overlay.java
+++ junit-platform-manager-base-8/src/main/java/de/sormuras/junit/platform/manager/Overlay.java
@@ -3,8 +3,22 @@ package de.sormuras.junit.platform.manager;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+/** Java version-aware helpers. */
 interface Overlay {
 
   /** Create new URLClassLoader instance. */
   URLClassLoader newClassLoader(String name, ClassLoader parent, URL... urls);
+
+  /** Log-related methods. */
+  interface Log {
+
+    /** Log a message formatted in {@link java.text.MessageFormat} style at debug level. */
+    void debug(String format, Object... args);
+
+    /** Log a message formatted in {@link java.text.MessageFormat} style at info level. */
+    void info(String format, Object... args);
+
+    /** Log a message formatted in {@link java.text.MessageFormat} style at warning level. */
+    void warn(String format, Object... args);
+  }
 }
--- junit-platform-manager-java-11/src/main/java/de/sormuras/junit/platform/manager/OverlaySingleton.java
+++ junit-platform-manager-java-11/src/main/java/de/sormuras/junit/platform/manager/OverlaySingleton.java
@@ -1,10 +1,31 @@
 package de.sormuras.junit.platform.manager;
 
+import static java.lang.System.Logger.Level.DEBUG;
+import static java.lang.System.Logger.Level.INFO;
+import static java.lang.System.Logger.Level.WARNING;
+
 import java.net.URL;
 import java.net.URLClassLoader;
 
-enum OverlaySingleton implements Overlay {
+enum OverlaySingleton implements Overlay, Overlay.Log {
   INSTANCE {
+    private final System.Logger logger = System.getLogger(Overlay.class.getPackage().getName());
+
+    @Override
+    public void debug(String format, Object... args) {
+      logger.log(DEBUG, format, args);
+    }
+
+    @Override
+    public void info(String format, Object... args) {
+      logger.log(INFO, format, args);
+    }
+
+    @Override
+    public void warn(String format, Object... args) {
+      logger.log(WARNING, format, args);
+    }
+
     @Override
     public URLClassLoader newClassLoader(String name, ClassLoader parent, URL... urls) {
       return new URLClassLoader(name, urls, parent);

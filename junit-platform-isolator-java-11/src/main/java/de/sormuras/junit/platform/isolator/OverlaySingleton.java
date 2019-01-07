520bf18a71f40c6bdf999ce639bbf2b8e2c90255
Christian Stein <sormuras@gmail.com>
2019-01-07 12:19:24 +0100

    Prepare `module-info.test` support
    Issue #10

--- junit-platform-isolator-java-11/src/main/java/de/sormuras/junit/platform/isolator/OverlaySingleton.java
+++ junit-platform-isolator-java-11/src/main/java/de/sormuras/junit/platform/isolator/OverlaySingleton.java
@@ -1,9 +1,13 @@
 package de.sormuras.junit.platform.isolator;
 
+import static java.lang.ModuleLayer.boot;
+import static java.lang.ModuleLayer.defineModulesWithOneLoader;
+
 import java.lang.module.ModuleFinder;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Set;
 
 public enum OverlaySingleton implements Overlay {
@@ -16,12 +20,17 @@ public enum OverlaySingleton implements Overlay {
     @Override
     public ClassLoader newModuleLoader(Set<String> modules, ClassLoader parent, Path... entries) {
       var finder = ModuleFinder.of(entries);
-      var bootLayer = ModuleLayer.boot();
-      var configuration = bootLayer.configuration().resolve(finder, ModuleFinder.of(), modules);
-      var parentLoader = ClassLoader.getSystemClassLoader();
-      var configuredLayer = bootLayer.defineModulesWithOneLoader(configuration, parentLoader);
+      var configuration = boot().configuration().resolve(finder, ModuleFinder.of(), modules);
+      var parentLayers = List.of(boot());
+      var parentLoader = ClassLoader.getSystemClassLoader(); // TODO ... parent?!
+      var controller = defineModulesWithOneLoader(configuration, parentLayers, parentLoader);
+      // TODO Read `module-info.test` to configure module layer at runtime
+      //   https://github.com/sormuras/junit-platform-isolator/issues/10
+      //   controller.addExports();
+      //   controller.addOpens();
+      //   controller.addReads();
       var name = "de.sormuras.junit.platform.isolator.worker"; // TODO Or one of modules?
-      return configuredLayer.findLoader(name);
+      return controller.layer().findLoader(name);
     }
 
     @Override

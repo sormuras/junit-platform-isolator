b197fabba0e9992d2a2a6b256302fb7b7dd20b28
Christian Stein <sormuras@gmail.com>
2019-01-10 11:06:46 +0100

    Remove configuration-related properties from Driver

--- junit-platform-isolator-base-8/src/main/java/de/sormuras/junit/platform/isolator/Driver.java
+++ junit-platform-isolator-base-8/src/main/java/de/sormuras/junit/platform/isolator/Driver.java
@@ -1,9 +1,5 @@
 package de.sormuras.junit.platform.isolator;
 
-import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
-
 public interface Driver {
 
   /** Log a message formatted in {@link java.text.MessageFormat} style at debug level. */
@@ -17,15 +13,4 @@ public interface Driver {
 
   /** Log a message formatted in {@link java.text.MessageFormat} style at error level. */
   void error(String format, Object... args);
-
-  /** Named paths sets. */
-  Map<String, Set<Path>> paths();
-
-  /** Modules helper. */
-  Modules modules();
-
-  /** If worker class could not be loaded in isolation: fail. */
-  default boolean isIllegalStateIfWorkerIsNotLoadedInIsolation() {
-    return true;
-  }
 }
--- junit-platform-isolator/src/test/java/NoopDriver.java
+++ junit-platform-isolator/src/test/java/NoopDriver.java
@@ -1,10 +1,4 @@
 import de.sormuras.junit.platform.isolator.Driver;
-import de.sormuras.junit.platform.isolator.Modules;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 
 class NoopDriver implements Driver {
   @Override
@@ -18,19 +12,4 @@ class NoopDriver implements Driver {
 
   @Override
   public void error(String format, Object... args) {}
-
-  @Override
-  public Map<String, Set<Path>> paths() {
-    return Collections.emptyMap();
-  }
-
-  @Override
-  public Modules modules() {
-    return new Modules(Paths.get("."), Paths.get("."));
-  }
-
-  @Override
-  public boolean isIllegalStateIfWorkerIsNotLoadedInIsolation() {
-    return false;
-  }
 }
